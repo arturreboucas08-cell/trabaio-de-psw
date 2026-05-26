d4c8dad32a30b8468d4fa6495c1227eab15e45a0
arturreboucas08-cell <arturreboucas08@gmail.com>
2026-05-26 15:11:18 -0300

    Add main content to index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+<?php include_once 'includes/header.php';?>
+<main>
+        <?php
+        echo"sou lindão, sou lindão"
+        ?>    
+    </main>
+    <?php include_once 'includes/footer.php';?>
 
